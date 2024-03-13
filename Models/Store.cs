dcb8fb4a3e29f8748445196c0cc29b1e35e1f88f
blumayut <122390259+blumayut@users.noreply.github.com>
2024-03-13 22:56:59 +0200

    Update Store.cs

--- Models/Store.cs
+++ Models/Store.cs
@@ -8,7 +8,7 @@
         public Guid Status  { get; set; }
         public string ?Description { get; set; }
         public User UserId { get; set; }
-        public ICColection<Categorie> Categorie { get; set; }
+        public IColection<Categorie> Categorie { get; set; }
         
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
         public bool ?IsShipping { get; set; }
